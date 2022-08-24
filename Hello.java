3dd9a0b59ed38c95ec09b16015cfa7323ba820c5
leakepa <111935465+leakepa@users.noreply.github.com>
2022-08-23 20:59:34 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/**************************************
+* Introduction Program
+***************************************
+* Philip Leake
+* 8/23/2022
+* CMSC-255-001
+***************************************/
+
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Philip Leake");
+		System.out.println("Introduction Program");
+		System.out.println("This program states name, name of program, date created, and course number");
+		System.out.println("8/23/2022");
+		System.out.println("CMSC-255-001");
+		System.out.println();
+	}
+}
